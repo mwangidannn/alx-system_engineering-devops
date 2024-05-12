d0375fb07974d5681ddd6ba01bb3d0f5912d4b18
@mwangidannn <duncanmwangikinuthia@gmail.com>
2024-05-13 02:55:09 +0300

    testing

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+#This is how to  Fixes bad `phpp` extensions 500 to `php` in the WordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
---
+++ 0x17-web_stack_debugging_3/README.md
@@ -0,0 +1,2 @@
+This  is my 0x17. Web stack debugging #3 tasks about web server and web stack 
+
